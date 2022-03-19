22328345e1700e3809b7a54a5fd7dd1025a14287
David Ybañez <dbybanez@gmail.com>
2022-03-19 23:22:51 +0800

    stylesheet import fixes

--- pages/index.vue
+++ pages/index.vue
@@ -1,9 +1,9 @@
 <template>
-  <Tutorial/>
+  <div>Just a home page</div>
 </template>
 
 <script>
-export default {
-  name: 'IndexPage'
-}
+// export default {
+//   name: 'IndexPage'
+// }
 </script>
