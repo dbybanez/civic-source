ef5ebdfaf989efb3fdbf904c4272bdd452a5f0a4
David Ybañez <dbybanez@gmail.com>
2022-03-20 02:39:42 +0800

    added images

--- pages/properties/_id/index.vue
+++ pages/properties/_id/index.vue
@@ -11,28 +11,28 @@
                     <div class="col">
                       <div class="d-block p-3">
                         <div class="row align-items-center">
-                          <div class="col">
-                            Paper image
+                          <div class="col text-center">
+                            <img src="~/assets/images/summary-list.png" style="width: 60px;">
                           </div>
-                          <div class="col">
+                          <div class="col text-center">
                             Property ID: {{ this.$route.params.id }}
                           </div>
-                          <div class="col">
-                            Map Marker image
+                          <div class="col text-center">
+                            <img src="~/assets/images/place-marker.png" style="width: 60px;">
                           </div>
                           <div class="col text-center">
                             <p class="mb-0">To <br/> Start<br/> Place Deposit</p>
                           </div>
-                          <div class="col">
-                            Bookmark image
+                          <div class="col text-center">
+                            <img src="~/assets/images/bookmark-ribbon.png" style="width: 60px;">
                           </div>
-                          <div class="col">
+                          <div class="col text-center">
                             <p class="mb-0">Market value $40,000</p>
                             <p class="mb-0">$5,000</p>
                             <p class="mb-0">Reserve Price</p>
                           </div>
-                          <div class="col">
-                            Manual image
+                          <div class="col text-center">
+                            <img src="~/assets/images/property-script.png" style="width: 60px;">
                           </div>
                         </div>
                       </div>
@@ -46,50 +46,50 @@
                     <div class="col">
                       <div class="d-block py-2 px-3">
                         <div class="row align-items-center">
-                          <div class="col">
-                            <p class="mb-0">Outbid <br/> 12</p>
+                          <div class="col-1">
+                            <p class="mb-0">Outbid <br/> <span class="text-danger">12</span> </p>
+                          </div>
+                          <div class="col-1">
+                            <p class="mb-0">Active <br/> <span class="text-secondary">24</span></p>
+                          </div>
+                          <div class="col-1">
+                            <p class="mb-0">Winning <br/> <span class="text-success">12</span></p>
                           </div>
                           <div class="col">
-                            <p class="mb-0">Active <br/> 24</p>
-                          </div>
-                          <div class="col">
-                            <p class="mb-0">Winning <br/> 12</p>
-                          </div>
-                          <div class="col">
-                            <p class="mb-0">14 Flood St</p>
+                            <p class="mb-0"><img src="~/assets/images/sort-up.png" style="width: 16px; filter: invert(35%) sepia(51%) saturate(6794%) hue-rotate(144deg) brightness(89%) contrast(80%);"> 14 Flood St</p>
                             <div class="d-flex">
-                              <div class="me-3">$1,000</div>
+                              <div class="me-3 text-danger">$1,000</div>
                               <div>$5,000</div>
                             </div>
                           </div>
                           <div class="col">
-                            <p class="mb-0">14 Flood St</p>
+                            <p class="mb-0"><img src="~/assets/images/sort-down.png" style="width: 16px; filter: invert(26%) sepia(55%) saturate(3589%) hue-rotate(338deg) brightness(92%) contrast(86%);"> 24 Washington Ave.</p>
                             <div class="d-flex">
-                              <div class="me-3">$1,000</div>
+                              <div class="me-3 text-danger">$1,000</div>
                               <div>$5,000</div>
                             </div>
                           </div>
                           <div class="col">
-                            <p class="mb-0">14 Flood St</p>
+                            <p class="mb-0"><img src="~/assets/images/sort-right.png" style="width: 16px; filter: invert(28%) sepia(17%) saturate(307%) hue-rotate(169deg) brightness(99%) contrast(88%);"> 289 Hempstead St</p>
                             <div class="d-flex">
                               <div class="me-3">$1,000</div>
                               <div>$5,000</div>
                             </div> 
                           </div>
                           <div class="col">
-                            <p class="mb-0">14 Flood St</p>
+                            <p class="mb-0"><img src="~/assets/images/sort-up.png" style="width: 16px; filter: invert(35%) sepia(51%) saturate(6794%) hue-rotate(144deg) brightness(89%) contrast(80%);"> 57 West Ave G.</p>
                             <div class="d-flex">
                               <div class="me-3">$1,000</div>
                               <div>$5,000</div>
                             </div>
                           </div>
-                          <div class="col">
+                          <div class="col-1">
                             <p class="mb-0">Winning <br/> $100,000</p>
                           </div>
-                          <div class="col">
+                          <div class="col-1">
                             <p class="mb-0">Active <br/> $100,000</p>
                           </div>
-                          <div class="col">
+                          <div class="col-1">
                             <p class="mb-0">Outbid <br/> $100,000</p>
                           </div>
                         </div>
--- pages/properties/index.vue
+++ pages/properties/index.vue
@@ -6,7 +6,7 @@
           <div class="col-8">
             <div class="row">
               <div class="col-3">
-                <div class="shadow-sm rounded p-3 mb-3">
+                <div class="shadow-sm rounded p-3 mb-3 bg-body">
                   <h5>Property Name One</h5>
                   <p>Some details about property name one.</p>
                   <nuxt-link :to="{ name: 'properties-id', params: { id: 1 }}" class="nuxt-link-exact-active nuxt-link-active btn btn-primary">
@@ -15,7 +15,7 @@
                 </div>
               </div>
               <div class="col-3">
-                <div class="shadow-sm rounded p-3 mb-3">
+                <div class="shadow-sm rounded p-3 mb-3 bg-body">
                   <h5>Property Name Two</h5>
                   <p>Some details about property name two.</p>
                   <nuxt-link :to="{ name: 'properties-id', params: { id: 2 }}" class="nuxt-link-exact-active nuxt-link-active btn btn-primary">
@@ -24,7 +24,7 @@
                 </div>
               </div>
               <div class="col-3">
-                <div class="shadow-sm rounded p-3 mb-3">
+                <div class="shadow-sm rounded p-3 mb-3 bg-body">
                   <h5>Property Name Three</h5>
                   <p>Some details about property name three.</p>
                   <nuxt-link :to="{ name: 'properties-id', params: { id: 3 }}" class="nuxt-link-exact-active nuxt-link-active btn btn-primary">
@@ -33,7 +33,7 @@
                 </div>
               </div>
               <div class="col-3">
-                <div class="shadow-sm rounded p-3 mb-3">
+                <div class="shadow-sm rounded p-3 mb-3 bg-body">
                   <h5>Property Name Four</h5>
                   <p>Some details about property name four.</p>
                   <nuxt-link :to="{ name: 'properties-id', params: { id: 4 }}" class="nuxt-link-exact-active nuxt-link-active btn btn-primary">
@@ -42,7 +42,7 @@
                 </div>
               </div>
               <div class="col-3">
-                <div class="shadow-sm rounded p-3 mb-3">
+                <div class="shadow-sm rounded p-3 mb-3 bg-body">
                   <h5>Property Name Five</h5>
                   <p>Some details about property name five.</p>
                   <nuxt-link :to="{ name: 'properties-id', params: { id: 5 }}" class="nuxt-link-exact-active nuxt-link-active btn btn-primary">
@@ -51,7 +51,7 @@
                 </div>
               </div>
               <div class="col-3">
-                <div class="shadow-sm rounded p-3 mb-3">
+                <div class="shadow-sm rounded p-3 mb-3 bg-body">
                   <h5>Property Name Six</h5>
                   <p>Some details about property name six.</p>
                   <nuxt-link :to="{ name: 'properties-id', params: { id: 6 }}" class="nuxt-link-exact-active nuxt-link-active btn btn-primary">
