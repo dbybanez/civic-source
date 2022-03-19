bb9f753b382626d98238554708be719aed54d232
David Ybañez <dbybanez@gmail.com>
2022-03-20 02:02:20 +0800

    minor UI fixes

--- pages/properties/_id/index.vue
+++ pages/properties/_id/index.vue
@@ -5,83 +5,93 @@
         <div class="row align-items-center h-100">
           <div class="col px-0 h-100">
             <div class="position-absolute h-100 w-100">
-              <div class="container-fluid" style="margin-top: 3.75rem;">
-                <div class="row">
-                  <div class="col">
-                    <div class="d-block bg-body p-3" style="--bs-bg-opacity: .8;">
-                      <div class="row align-items-center">
-                        <div class="col">
-                          Paper image
-                        </div>
-                        <div class="col">
-                          Property ID: {{ this.$route.params.id }}
-                        </div>
-                        <div class="col">
-                          Map Marker image
-                        </div>
-                        <div class="col text-center">
-                          <p class="mb-0">To <br/> Start<br/> Place Deposit</p>
-                        </div>
-                        <div class="col">
-                          Bookmark image
-                        </div>
-                        <div class="col">
-                          <p class="mb-0">Market value $40,000</p>
-                          <p class="mb-0">$5,000</p>
-                          <p class="mb-0">Reserve Price</p>
-                        </div>
-                        <div class="col">
-                          Manual image
+              <div class="d-block bg-body" style="--bs-bg-opacity: .8;">
+                <div class="container-fluid" style="margin-top: 3.75rem;">
+                  <div class="row">
+                    <div class="col">
+                      <div class="d-block p-3">
+                        <div class="row align-items-center">
+                          <div class="col">
+                            Paper image
+                          </div>
+                          <div class="col">
+                            Property ID: {{ this.$route.params.id }}
+                          </div>
+                          <div class="col">
+                            Map Marker image
+                          </div>
+                          <div class="col text-center">
+                            <p class="mb-0">To <br/> Start<br/> Place Deposit</p>
+                          </div>
+                          <div class="col">
+                            Bookmark image
+                          </div>
+                          <div class="col">
+                            <p class="mb-0">Market value $40,000</p>
+                            <p class="mb-0">$5,000</p>
+                            <p class="mb-0">Reserve Price</p>
+                          </div>
+                          <div class="col">
+                            Manual image
+                          </div>
                         </div>
                       </div>
                     </div>
-                    <div class="d-block bg-body py-2 px-3" style="--bs-bg-opacity: .5;">
-                      <div class="row align-items-center">
-                        <div class="col">
-                          <p class="mb-0">Outbid <br/> 12</p>
-                        </div>
-                        <div class="col">
-                          <p class="mb-0">Active <br/> 24</p>
-                        </div>
-                        <div class="col">
-                          <p class="mb-0">Winning <br/> 12</p>
-                        </div>
-                        <div class="col">
-                          <p class="mb-0">14 Flood St</p>
-                          <div class="d-flex">
-                            <div class="me-3">$1,000</div>
-                            <div>$5,000</div>
+                  </div>
+                </div>
+              </div>
+              <div class="d-block bg-body" style="--bs-bg-opacity: .5;">
+                <div class="container-fluid">
+                  <div class="row">
+                    <div class="col">
+                      <div class="d-block py-2 px-3">
+                        <div class="row align-items-center">
+                          <div class="col">
+                            <p class="mb-0">Outbid <br/> 12</p>
                           </div>
-                        </div>
-                        <div class="col">
-                          <p class="mb-0">14 Flood St</p>
-                          <div class="d-flex">
-                            <div class="me-3">$1,000</div>
-                            <div>$5,000</div>
+                          <div class="col">
+                            <p class="mb-0">Active <br/> 24</p>
                           </div>
-                        </div>
-                        <div class="col">
-                          <p class="mb-0">14 Flood St</p>
-                          <div class="d-flex">
-                            <div class="me-3">$1,000</div>
-                            <div>$5,000</div>
-                          </div> 
-                        </div>
-                        <div class="col">
-                          <p class="mb-0">14 Flood St</p>
-                          <div class="d-flex">
-                            <div class="me-3">$1,000</div>
-                            <div>$5,000</div>
+                          <div class="col">
+                            <p class="mb-0">Winning <br/> 12</p>
                           </div>
-                        </div>
-                        <div class="col">
-                          <p class="mb-0">Winning <br/> $100,000</p>
-                        </div>
-                        <div class="col">
-                          <p class="mb-0">Active <br/> $100,000</p>
-                        </div>
-                        <div class="col">
-                          <p class="mb-0">Outbid <br/> $100,000</p>
+                          <div class="col">
+                            <p class="mb-0">14 Flood St</p>
+                            <div class="d-flex">
+                              <div class="me-3">$1,000</div>
+                              <div>$5,000</div>
+                            </div>
+                          </div>
+                          <div class="col">
+                            <p class="mb-0">14 Flood St</p>
+                            <div class="d-flex">
+                              <div class="me-3">$1,000</div>
+                              <div>$5,000</div>
+                            </div>
+                          </div>
+                          <div class="col">
+                            <p class="mb-0">14 Flood St</p>
+                            <div class="d-flex">
+                              <div class="me-3">$1,000</div>
+                              <div>$5,000</div>
+                            </div> 
+                          </div>
+                          <div class="col">
+                            <p class="mb-0">14 Flood St</p>
+                            <div class="d-flex">
+                              <div class="me-3">$1,000</div>
+                              <div>$5,000</div>
+                            </div>
+                          </div>
+                          <div class="col">
+                            <p class="mb-0">Winning <br/> $100,000</p>
+                          </div>
+                          <div class="col">
+                            <p class="mb-0">Active <br/> $100,000</p>
+                          </div>
+                          <div class="col">
+                            <p class="mb-0">Outbid <br/> $100,000</p>
+                          </div>
                         </div>
                       </div>
                     </div>
